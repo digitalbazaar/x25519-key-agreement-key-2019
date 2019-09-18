0851376d77a950c59c86bfac78f5c4f4cdae9fb5
David I. Lehn <dlehn@digitalbazaar.com>
2019-09-18 13:48:48 -0400

    Add eslint support and fix lint issues.

--- tests/X25519KeyPair.spec.js
+++ tests/X25519KeyPair.spec.js
@@ -16,8 +16,9 @@ describe('X25519KeyPair', () => {
     it('should convert both public and private key', async () => {
       const edKeyPair = await Ed25519KeyPair.from({
         controller: 'did:example:123',
-        "privateKeyBase58": "4F71TAGqQYe7KE9p4HUzoVV9arQwKP4gPtvi89EPNGuwA1qLE4RRxitA2rEcdEszERj3pN1DWKARBZQ2BACLbW1V",
-        "publicKeyBase58": "HLi1h9SzENZyEv7ifPNtu8xyJNzCFFeaC6X9rsZKFgv3"
+        /* eslint-disable-next-line max-len */
+        privateKeyBase58: '4F71TAGqQYe7KE9p4HUzoVV9arQwKP4gPtvi89EPNGuwA1qLE4RRxitA2rEcdEszERj3pN1DWKARBZQ2BACLbW1V',
+        publicKeyBase58: 'HLi1h9SzENZyEv7ifPNtu8xyJNzCFFeaC6X9rsZKFgv3'
       });
 
       const xKeyPair = X25519KeyPair.fromEdKeyPair(edKeyPair);
@@ -34,12 +35,12 @@ describe('X25519KeyPair', () => {
   describe('deriveSecret', () => {
     it('should produce a secret from a remote key', async () => {
       const localKey = await X25519KeyPair.from({
-        "privateKeyBase58": "B1tfmsThxDBrFx7VdtimC26s1WW1aFySxdR16n5SfDJa",
-        "publicKeyBase58": "FWzRdFAfTJGsdPWFvD1oXy469wAsGptMiFpdecxgcek6"
+        privateKeyBase58: 'B1tfmsThxDBrFx7VdtimC26s1WW1aFySxdR16n5SfDJa',
+        publicKeyBase58: 'FWzRdFAfTJGsdPWFvD1oXy469wAsGptMiFpdecxgcek6'
       });
 
       const remoteKey = await X25519KeyPair.from({
-        "publicKeyBase58": "73e843su1epHouuHyDzjy2YXZfZrNiXLrr1hjpJkBeUG"
+        publicKeyBase58: '73e843su1epHouuHyDzjy2YXZfZrNiXLrr1hjpJkBeUG'
       });
 
       const secret = localKey.deriveSecret({publicKey: remoteKey});
@@ -66,6 +67,6 @@ describe('X25519KeyPair', () => {
       const result = key.verifyFingerprint(fingerprint);
       expect(result.valid).to.be.true;
       expect(result.error).to.not.exist;
-    })
+    });
   });
 });
